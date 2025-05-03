c6ebd411cdcaba6930b1291717503d18342276c7
Habib-u <mdhera211@gmail.com>
2025-05-03 02:18:13 -0400

    Refactor code structure for improved readability and maintainability

--- server/main.py
+++ server/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from server!")
+from fastapi import FastAPI, WebSocket
+from socket_pro import socketio
+import uvicorn
+
+app = FastAPI()
+socketconn = socketio()
+
+
+@app.websocket("/ws/{client_id}")
+async def home(websocke: WebSocket, client_id: int):
+    await socketconn.connect(websocket=websocke)
+    print(f"connected {client_id}")
+    try:
+        while True:
+            data = await websocke.receive_json()
+            print(data)
+
+    except:
+        socketconn.disconnect(websocket=websocke)
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
